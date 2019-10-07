781325f5bbcdc4bc40316fd5af11f4013217868d
Matt Calhoun <matt@gruntwork.io>
2019-10-07 18:49:40 -0400

    Update cmd/helm.go
    Co-Authored-By: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- cmd/helm.go
+++ cmd/helm.go
@@ -188,7 +188,7 @@ var (
 	// This is also used in configure
 	helmHomeFlag = cli.StringFlag{
 		Name:   "helm-home",
-		Usage:  "Home directory that is configured for accessing deployed Tiller server. (default: $HELM_HOME or \"~/.helm\")",
+		Usage:  "Home directory that is configured for accessing deployed Tiller server. You can also set this using the environment variable HELM_HOME. (default: \"~/.helm\")",
 		EnvVar: "HELM_HOME",
 	}
 
