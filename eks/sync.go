4c35fb9e1d51b218ad5ac2ab842fa3bbe2a0a5fd
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-04-14 09:05:27 -0500

    Add support for 1.22 and drop support of 1.18, 1.17, and 1.16

--- eks/sync.go
+++ eks/sync.go
@@ -39,36 +39,30 @@ const (
 
 var (
 	// NOTE: Ensure that there is an entry for each supported version in the following tables.
-	supportedVersions = []string{"1.21", "1.20", "1.19", "1.18", "1.17", "1.16"}
+	supportedVersions = []string{"1.22", "1.21", "1.20", "1.19"}
 
 	// Reference: https://docs.aws.amazon.com/eks/latest/userguide/managing-coredns.html
 	coreDNSVersionLookupTable = map[string]string{
+		"1.22": "1.8.7-eksbuild",
 		"1.21": "1.8.4-eksbuild",
 		"1.20": "1.8.3-eksbuild",
 		"1.19": "1.8.0-eksbuild",
-		"1.18": "1.7.0-eksbuild",
-		"1.17": "1.6.6-eksbuild",
-		"1.16": "1.6.6-eksbuild",
 	}
 
 	// Reference: https://docs.aws.amazon.com/eks/latest/userguide/managing-kube-proxy.html#updating-kube-proxy-add-on
 	kubeProxyVersionLookupTable = map[string]string{
+		"1.22": "1.22.6-eksbuild",
 		"1.21": "1.21.2-eksbuild",
 		"1.20": "1.20.4-eksbuild",
 		"1.19": "1.19.6-eksbuild",
-		"1.18": "1.18.8-eksbuild",
-		"1.17": "1.17.9-eksbuild",
-		"1.16": "1.16.13-eksbuild",
 	}
 
 	// Reference: https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html
 	amazonVPCCNIVersionLookupTable = map[string]string{
-		"1.21": "1.9.0",
-		"1.20": "1.9.0",
-		"1.19": "1.9.0",
-		"1.18": "1.9.0",
-		"1.17": "1.9.0",
-		"1.16": "1.9.0",
+		"1.22": "1.10.2",
+		"1.21": "1.10.2",
+		"1.20": "1.10.2",
+		"1.19": "1.10.2",
 	}
 
 	defaultContainerImageAccount = "602401143452"
